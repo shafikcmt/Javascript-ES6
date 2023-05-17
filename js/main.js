5cc4d584c486ffd7c66cb4f669864d10aef20081
shafikcmt <mdshafiqulislam822@gmail.com>
2023-05-17 14:49:55 +0530

    Var and let keyword example

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 var x1 = 3;
-let x2 = 5;
-document.write(x1+"<br>");
-document.write(x2+"<br>");
+let x2 = 50;
+{
+    var a = 100;
+    let x = 30;
+    document.write("Inside with let keyword:"+x+"<br>");
+    document.write("Inside with let keyword global:"+x1+"<br>");
+    document.write("Inside with var keyword:"+a);
+    
+    
+}
+
+document.write("Outside with let keyword:"+x); // not working because its declare in scope
+document.write("Outside with let keyword:"+x3);
+
